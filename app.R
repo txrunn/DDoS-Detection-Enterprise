6c1e5a9b0e7ba899dd4e0e77ee70d52d2a6ac70e
Tarun Gunaseelan <tarun.gunaseelan@gmail.com>
2023-03-25 16:08:49 -0400

    Created Shiny WebApp skeleton

---
+++ app.R
@@ -0,0 +1,12 @@
+library(shiny)
+source("ddos_detection.R")
+
+ui <- fluidPage(
+  # Add UI elements for input parameters here
+)
+
+server <- function(input, output, session) {
+  # Add server-side
+}
+
+shinyApp(ui, server)
---
+++ ddos_detection.R
@@ -0,0 +1,35 @@
+# Load necessary libraries
+library(aws.ec2)
+library(aws.s3)
+library(caret)
+
+# Function to deploy the CloudFormation template
+deploy_cloudformation_template <- function(template_file, stack_name, aws_account) {
+  # Add code to deploy the CloudFormation template to the specified AWS account
+}
+
+# Function to simulate and collect network traffic data
+collect_network_traffic_data <- function() {
+  # Add code to simulate DDoS attacks and collect network traffic data
+}
+
+# Function to preprocess network traffic data
+preprocess_data <- function(raw_data) {
+  # Add code to preprocess the raw network traffic data
+}
+
+# Function to train the machine learning model
+train_ddos_detection_model <- function(preprocessed_data, algorithm_type, seed_number) {
+  # Add code to train the machine learning model based on the selected algorithm and seed number
+}
+
+# Function to evaluate the model's performance
+evaluate_model_performance <- function(model, test_data) {
+  # Add code to evaluate the model's performance using the test data
+}
+
+# Function to deploy the trained model
+deploy_trained_model <- function(model) {
+  # Add code to deploy the trained model for real-time DDoS attack detection
+}
+
